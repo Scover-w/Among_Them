4763388402049c80cc507725f0fe9907b5a6da9e
Scover-w <scover_contact@protonmail.com>
2021-03-29 22:15:37 +0200

    News objects in Blender File

---
+++ AmongThemUnity/Assets/Scripts/UI/FPSCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCount : MonoBehaviour
+{
+    [SerializeField] 
+    private Text fps;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        fps.text = (1/Time.deltaTime).ToString();
+    }
+}
