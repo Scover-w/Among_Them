3501d7e9d144fdc2d972c2355209b32ea7f58fd0
Scover-w <scover_contact@protonmail.com>
2021-05-02 20:56:50 +0200

    Prefab creator in editor

---
+++ AmongThemUnity/Assets/Scripts/Editor/PrefabCreator.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class PrefabCreator : UnityEditor.Editor
+{
+    private static string prefabPath = $@"\Prefabs\ProceduralEntities\";
+    private static string blenderObjectPath = $@"\BlenderImport\";
+    
+    [MenuItem("Prefabs/Create")]
+    public static void CreatePrefab()
+    {
+        RecursivePrefabCreator("Assets" + blenderObjectPath);
+    }
+
+
+    static void RecursivePrefabCreator(string path)
+    {
+        string[] files = Directory.GetFiles(path);
+        string[] directories = Directory.GetDirectories(path);
+
+        foreach (var file in files)
+        {
+            if (file.Contains(".meta"))
+                continue;
+            
+            var prefabFile = file.Replace(blenderObjectPath, prefabPath).Replace(".fbx", ".prefab");
+            if (!File.Exists(prefabFile))
+            {
+                var objectToPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(file, typeof(GameObject));
+                objectToPrefab = Instantiate(objectToPrefab);
+                objectToPrefab.AddComponent<ProceduralEntity>();
+                objectToPrefab.transform.position = Vector3.zero;
+
+                PrefabUtility.SaveAsPrefabAssetAndConnect(objectToPrefab, prefabFile, InteractionMode.UserAction);
+                DestroyImmediate(objectToPrefab);
+                Debug.Log(prefabFile + " created.");
+            }
+        }
+
+        foreach (var directory in directories)
+        {
+            RecursivePrefabCreator(directory);
+        }
+        
+    }
+    
+    // https://docs.unity3d.com/ScriptReference/EditorUtility.html
+    
+    // https://docs.unity3d.com/ScriptReference/PrefabUtility.html
+    
+    // https://docs.unity3d.com/ScriptReference/AssetDatabase.html
+    
+}
