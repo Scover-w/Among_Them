6ffdfd5fd263558a8773f8934c5126dd8ad4404a
kylyon <kmariemagdelaine@myges.fr>
2021-05-09 11:06:22 +0200

    Début Mission InGame Kyllian

---
+++ AmongThemUnity/Assets/Scripts/Missions/CodeMission.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeMission : MonoBehaviour
+{
+    public static CodeMission Instance() {return _singleton;}
+    private static CodeMission _singleton;
+
+    private string code = "0000";
+    void Start()
+    {
+        _singleton = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetCode()
+    {
+        return code;
+    }
+}
---
+++ AmongThemUnity/Assets/Scripts/Missions/CodePanelUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodePanelUI : MonoBehaviour
+{
+    public InputField codeDisplay;
+    public string number;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddToDisplay()
+    {
+        if (codeDisplay.text.Length < 4)
+        {
+            codeDisplay.text += number;
+        }
+    }
+
+    public void RemoveFromDisplay()
+    {
+        if (codeDisplay.text.Length > 0)
+        {
+            codeDisplay.text = codeDisplay.text.Remove(codeDisplay.text.Length-1);
+        }
+    }
+
+    public void ConfirmCode()
+    {
+        if (codeDisplay.text.Equals(CodeMission.Instance().GetCode()))
+        {
+            codeDisplay.text = "TRUE";
+            return;
+        }
+        codeDisplay.text = "FALSE";
+    }
+}
