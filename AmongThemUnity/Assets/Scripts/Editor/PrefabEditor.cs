41e848f5358ce92cb15d4846ab4e5fa6f3fd18c4
Scover-w <scover_contact@protonmail.com>
2021-06-05 18:59:33 +0200

    Tool Add MeshCollider to every prefab

---
+++ AmongThemUnity/Assets/Scripts/Editor/PrefabEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class PrefabEditor : Editor
+{
+    private static string _prefabPath = $@"\Prefabs\";
+    
+    [MenuItem("Tools/Prefabs/AddMeshCollider")]
+    public static void AddMeshCollider()
+    {
+        RecursiveAddMeshCollider("Assets" + _prefabPath);
+    }
+
+    public static void RecursiveAddMeshCollider(string path)
+    {
+        string[] files = Directory.GetFiles(path);
+        string[] directories = Directory.GetDirectories(path);
+
+        MeshCollider meshCollider;
+        MeshCollider meshColliderChild;
+        
+        foreach (var file in files)
+        {
+            if (file.Contains(".meta"))
+                continue;
+            
+            
+            var prefab = (GameObject)AssetDatabase.LoadAssetAtPath(file, typeof(GameObject));
+            
+            meshCollider = prefab.GetComponent<MeshCollider>();
+
+            if (meshCollider == null)
+                prefab.AddComponent<MeshCollider>();
+            
+            foreach (Transform child in prefab.transform)
+            {
+                meshColliderChild = child.GetComponent<MeshCollider>();
+                
+                if(meshColliderChild == null)
+                    child.gameObject.AddComponent<MeshCollider>();
+            }
+        }
+
+        foreach (var directory in directories)
+        {
+            RecursiveAddMeshCollider(directory);
+        }
+    }
+}
--- AmongThemUnity/Assets/Scripts/Materials/RandomMaterials.cs
+++ AmongThemUnity/Assets/Scripts/Materials/RandomMaterials.cs
@@ -16,11 +16,8 @@ public class RandomMaterials : MonoBehaviour
         // HSV color
         float firstColor = Random.Range(0f, 360f);
         float secondColor = (firstColor < 180f) ? firstColor + 180f : firstColor - 180f; // Complementary color
-        Debug.Log(firstColor);
-        Debug.Log(secondColor);
 
         Color rgbColor = Color.HSVToRGB(firstColor, 86.7f, 88.2f);
-        Debug.Log("rgb color : " + rgbColor.r + " " + rgbColor.g + " " + rgbColor.b);
         objectMeshRender.sharedMaterial.color = Color.HSVToRGB(firstColor / 360f, 86.7f / 100f, 88.2f / 100f);
         
         envMeshRenderer.sharedMaterial.color =  Color.HSVToRGB(secondColor / 360f, 86.7f / 100f, 88.2f / 100f);
