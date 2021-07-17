d3a57968aab58a5d804f7af9aa9c3e4879717525
Scover-w <scover_contact@protonmail.com>
2021-07-17 22:00:30 +0200

    Begin cinematic

---
+++ AmongThemUnity/Assets/Scripts/Cinematic/CinematicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(WaitEndVideo));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitEndVideo()
+    {
+        yield return new WaitForSeconds(113f);
+        Debug.Log("EndVideo");
+    }
+}
