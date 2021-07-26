5087cbeff726464035f43848273f03985d1e1c73
Scover-w <scover_contact@protonmail.com>
2021-07-26 22:03:52 +0200

    Foreign Voices cinematic

--- AmongThemUnity/Assets/Scripts/Cinematic/CinematicManager.cs
+++ AmongThemUnity/Assets/Scripts/Cinematic/CinematicManager.cs
@@ -5,16 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class CinematicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] 
+    private SoundManager soundManager;
     void Start()
     {
         StartCoroutine(nameof(WaitEndVideo));
+
+        StartCoroutine(nameof(StartVoice));
     }
     
     IEnumerator WaitEndVideo()
     {
         yield return new WaitForSeconds(113f); // 113f
         SceneManager.LoadScene("RandomGamePlay", LoadSceneMode.Single);
+    }
+
+    IEnumerator StartVoice()
+    {
+        yield return null;
+        string language = LanguageManager.Instance().GetSelectedLanguage();
+        if (language == "fr")
+        {
+            soundManager.Play("FrenchVoice");
+        }
+        else
+        {
+            soundManager.Play("EnglishVoice");
+        }
     }
 
     public void SkipVideo()
